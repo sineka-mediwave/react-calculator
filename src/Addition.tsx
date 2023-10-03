67d5a53ba2d25561180c3a512306ee8f7234542f
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-03 19:17:18 +0530

    add using switch statement

---
+++ src/Addition.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+
+function MyButton(){
+    return <div>
+        <button>Add</button>
+        <button>Sub</button>
+        <button>Mul</button>
+        <button>Div</button>
+        
+        </div>;
+}
+
+
+export default MyButton;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [num1, getFirstValue] = useState('');
+  const [num2, getSecondValue] = useState('');
+  const [outres, setOutres] = useState(0);
+
+  const fnHandler = (val: string) => {
+    const result = setOutput(val);
+    if (result) (setOutres(result));
+  }
+  function setOutput(val: string) {
+    switch (val) {
+      case '+':
+        return Number(num1) + Number(num2);
+        break;
+      case '-':
+        return Number(num1) - Number(num2);
+        break;
+      case '*':
+        return Number(num1) * Number(num2);
+        break;
+      case '/':
+        return Number(num1) / Number(num2);
+        break;
+      default:
+        return undefined;
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <div className='input-div'>
+        <label htmlFor="num1"> Enter First value: </label>
+        <input type="number" id='num1'
+          onChange={e => getFirstValue(e.target.value)}
+          value={num1} />
+      </div>
+      <div className='input-div'>
+        <label htmlFor="num2">Enter Second value: </label>
+        <input type="number" id='num2'
+          onChange={e => getSecondValue(e.target.value)}
+          value={num2} />
+      </div>
+      <div>
+        <button onClick={() => fnHandler('+')}>Add</button>
+        <button onClick={() => fnHandler('-')}>Sub</button>
+        <button onClick={() => fnHandler('*')}>Mul</button>
+        <button onClick={() => fnHandler('/')}>Div</button>
+      </div>
+      <div id='result'>{outres}</div>
+    </header>
   );
 }
 
