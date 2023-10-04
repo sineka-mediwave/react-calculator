4be0963751f7b92fee0f8ded7fca5f4c53d4dd3f
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-04 14:11:20 +0530

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,46 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [num1, getFirstValue] = useState('');
-  const [num2, getSecondValue] = useState('');
+  const [num1, getFirstValue] = useState("");
+  const [num2, getSecondValue] = useState("");
   const [outres, setOutres] = useState(0);
 
   const fnHandler = (val: string) => {
-    if(num1&&num2){
-
-      const result = eval(num1+ val +num2);
-      if (result) (setOutres(result));
+    if (num1 && num2) {
+      const result = eval(num1 + val + num2);
+      if (result) setOutres(result);
     }
-  }
-  
+  };
+
   return (
     <header className="App-header">
-      <div className='input-div'>
+      <div className="input-div">
         <label htmlFor="num1"> Enter First value: </label>
-        <input type="number" id='num1'
-          onChange={e => getFirstValue(e.target.value)}
-          value={num1} />
+        <input
+          type="number"
+          id="num1"
+          onChange={(e) => getFirstValue(e.target.value)}
+          value={num1}
+        />
       </div>
-      <div className='input-div'>
+      <div className="input-div">
         <label htmlFor="num2">Enter Second value: </label>
-        <input type="number" id='num2'
-          onChange={e => getSecondValue(e.target.value)}
-          value={num2} />
+        <input
+          type="number"
+          id="num2"
+          onChange={(e) => getSecondValue(e.target.value)}
+          value={num2}
+        />
       </div>
+
       <div>
-        <button onClick={() => fnHandler('+')}>Add</button>
-        <button onClick={() => fnHandler('-')}>Sub</button>
-        <button onClick={() => fnHandler('*')}>Mul</button>
-        <button onClick={() => fnHandler('/')}>Div</button>
+        <button onClick={() => fnHandler("+")}>Add</button>
+        <button onClick={() => fnHandler("-")}>Sub</button>
+        <button onClick={() => fnHandler("*")}>Mul</button>
+        <button onClick={() => fnHandler("/")}>Div</button>
       </div>
-      <div id='result'>{outres}</div>
+      <div id="result">{outres}</div>
     </header>
   );
 }
